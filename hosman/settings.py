6671a6000e9d56391224a175882b6e1d6a7f66d4
akash-2004 <akashk.projects@gmail.com>
2024-09-08 00:39:39 +0530

    Reverting back from Postgresql to dbsqlite due to timeout issues

--- hosman/settings.py
+++ hosman/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2giciol1k#=)o@^gs&rd(9xxkcjt%6hdks+_17l3&1$_@i9peo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
