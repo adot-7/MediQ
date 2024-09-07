4d4705797c57773047135c4fdc64ecea821a4b78
akash-2004 <akashk.projects@gmail.com>
2024-09-08 01:37:00 +0530

    Added fixes

--- hosman/settings.py
+++ hosman/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoprojec
 t.com/en/5.1/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 
+#not needed cause vercel env. 
+# from dotenv import load_dotenv
+# load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2giciol1k#=)o@^gs&rd(9xxkcjt%6hdks+_17l3&1$_@i9peo'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
