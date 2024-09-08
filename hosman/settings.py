ac48601414ec6e28fc9b1f108ddcbd47e34c5c90
akash-2004 <akashk.projects@gmail.com>
2024-09-08 22:50:56 +0530

    Added frontend v1

--- hosman/settings.py
+++ hosman/settings.py
@@ -85,16 +85,14 @@ WSGI_APPLICATION = 'hosman.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default':  dj_database_url.config(default=os.getenv('DATABASE_URL'))
-
-    # {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('PGDATABASE'),
-    #     'USER': os.getenv('PGUSER'),
-    #     'PASSWORD': os.getenv('PGPASSWORD'),
-    #     'HOST': os.getenv('HOST'),
-    #     'PORT': os.getenv('PORT'),
-    # }
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+    }
 }
 
 # Security settings
